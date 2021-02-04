7422cf5e3070244d9dffd0bee7e15e44acf5f690
max76667 <max766667@gmail.com>
2021-02-05 00:03:02 +0600

    feat: добавил функцию создания карточек

--- js/index.js
+++ js/index.js
@@ -1,31 +1,117 @@
-let popupForm = document.querySelector('.popup__form'); 
-let editButton = document.querySelector('.profile__edit-button');
-let saveButton = document.querySelector('.popup__save-button');
+const page = document.querySelector('.page');
+const editButton = document.querySelector('.profile__edit-button');
+let submitButton = document.querySelector('.popup__submit-button');
 let closeButton = document.querySelector('.popup__close-button');
+const addButton = document.querySelector('.profile__add-button');
 let popup = document.querySelector('.popup');
 let popupName = document.querySelector('.popup__input_type_name');
 let popupPosition = document.querySelector('.popup__input_type_position');
-let profileName = document.querySelector('.profile__name');
-let profilePosition = document.querySelector('.profile__position');
+let popupForm = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profilePosition = document.querySelector('.profile__position');
+const cardsContainer = document.querySelector('.cards__container');
+const cardTemplate = document.querySelector('#card').content;
+const popupTemplate = document.querySelector('#popup').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const popupTypeEdit = {
+  title: 'Редактировать профиль',
+  nameInput: 'Ваше имя',
+  positionInput: 'Ваша должность',
+  submit: 'Сохранить'
+};
+
+const popupTypeAdd = {
+  title: 'Новое место',
+  nameInput: 'Название',
+  positionInput: 'Ссылка на картинку',
+  submit: 'Создать'
+};
 
 function formSubmit(event) {
   event.preventDefault();
-  profileName.textContent = popupName.value;
-  profilePosition.textContent = popupPosition.value;
+  if(event.target.textContent.trim() == popupTypeEdit.submit) {
+    profileName.textContent = popupName.value;
+    profilePosition.textContent = popupPosition.value;
+  }
+  if(event.target.textContent.trim() == popupTypeAdd.submit) {
+    addCard({});
+  }
   closePopup();
+}
+
+function createPopup(popupType) {
+  const popupElement = popupTemplate.querySelector('.popup').cloneNode(true);
+
+  popupElement.querySelector('.popup__title').textContent = popupType.title;
+  popupElement.querySelector('.popup__input_type_name').placeholder = popupType.nameInput;
+  popupElement.querySelector('.popup__input_type_position').placeholder = popupType.positionInput;
+  popupElement.querySelector('.popup__submit-button').textContent = popupType.submit;
+
+  page.append(popupElement);
+  
+  popup = document.querySelector('.popup');
+  popupName = document.querySelector('.popup__input_type_name');
+  popupPosition = document.querySelector('.popup__input_type_position');
+  popupForm = document.querySelector('.popup__form');
+  closeButton = document.querySelector('.popup__close-button');
+  popupForm.addEventListener('submit', formSubmit);
+  closeButton.addEventListener('click', closePopup);
 }
 
 function openPopup(event) {
   event.preventDefault();
-  popupName.value = profileName.textContent;
-  popupPosition.value = profilePosition.textContent;
+  if(event.target === editButton) {
+    createPopup(popupTypeEdit);
+    popupName.value = profileName.textContent;
+    popupPosition.value = profilePosition.textContent;
+  }
+  if(event.target === addButton) {
+    createPopup(popupTypeAdd);
+  }
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.remove();
 }
 
-popupForm.addEventListener('submit', formSubmit);
+function addCard(props) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = props.name;
+  cardElement.querySelector('.card__image').src = props.link;
+
+  cardsContainer.append(cardElement);
+}
+
+initialCards.forEach(addCard);
+
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+
+addButton.addEventListener('click', openPopup);
